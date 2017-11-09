b4cbab54d280bdd80fc2a1b4f387e94cc9667ec8
Paul Arndt <paul.arndt92@gmail.com>
2017-11-09 14:23:51 +0100

    Refactor download_data.py, remove jupyter notebooks

---
+++ scripts/download_data.py
@@ -0,0 +1,47 @@
+import urllib.request
+import os
+import zipfile
+import sys
+#sys.path.insert(0, './parse_data_scripts')
+
+urls = {
+	"dataset1": {
+		"parse": False,
+	},
+	"dataset2": {
+		"url":"https://www.dropbox.com/sh/1v31yeu0zb4jcmm/AAAi5medvxa_kIbAEB7Edywua?dl=1&preview=LargeSets.zip",
+		"parse": False,
+	},
+	"dataset3":	{
+		"url":"https://www.dropbox.com/s/0hqq2g2ipefy1u1/large_allSampleTypes.zip?dl=1",
+		"parse": True,
+	}
+}
+
+if not os.path.exists("data"):
+	os.makedirs("data")
+
+for dataset, url in urls.items():
+	path = "data/" + dataset
+	if "url" in url:
+		if not os.path.exists(path):
+			print("download " + dataset)
+			os.makedirs(path)
+			urllib.request.urlretrieve(url["url"], path + "/download.zip")
+			print("unzip " + dataset)
+			with zipfile.ZipFile(path + "/download.zip","r") as zip_ref:
+				zip_ref.extractall(path)
+			os.remove(path + "/download.zip")
+		else:
+			print("skip already downloaded " + dataset)
+
+	if not os.path.exists(path + "/subsets"):
+		if url["parse"]:
+			print("parse " + dataset)
+			__import__("parse_" + dataset)
+´		else:
+			print("parser for " + dataset + " not implemented yet. Stay tuned!")
+	else:
+		print("already parsed " + dataset)
+
+	print("finished " + dataset)
