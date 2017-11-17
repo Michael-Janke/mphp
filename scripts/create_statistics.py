3c118a22ccdf01f690eea0ef02cc69e50bc0f81b
Tamara Slosarek <tamaraslosarek@gmail.com>
2017-11-17 11:08:31 +0100

    Create scripts with downloading data

--- scripts/create_statistics.py
+++ scripts/create_statistics.py
@@ -4,116 +4,116 @@ import os
 import matplotlib.pyplot as plt
 import argparse
 
-parser = argparse.ArgumentParser(description='Create statistics for sample types.')
-parser.add_argument('--name', help='name of dataset')
-args = parser.parse_args()
+def create_statistics(dataset):
+    parser = argparse.ArgumentParser(description='Create statistics for sample types.')
+    parser.add_argument('--name', help='name of dataset')
+    args = parser.parse_args()
 
-dataset = args.name
-METADATA_PATHS = {
-    "dataset3": "TCGA-GBM_TCGA-THCA_TCGA-LAML_TCGA-HNSC_TCGA-LUAD_TCGA-UCEC_TCGA-KIRC_TCGA-SARC__GeneExpressionQuantification_HTSeq-Counts_metadata.csv",
-    "dataset4": "TCGA-GBM_TCGA-THCA_TCGA-LAML_TCGA-HNSC_TCGA-LUAD_TCGA-UCEC_TCGA-KIRC_TCGA-SARC__GeneExpressionQuantification_HTSeq-Counts_metadata.csv",
-}
+    METADATA_PATHS = {
+        "dataset3": "TCGA-GBM_TCGA-THCA_TCGA-LAML_TCGA-HNSC_TCGA-LUAD_TCGA-UCEC_TCGA-KIRC_TCGA-SARC__GeneExpressionQuantification_HTSeq-Counts_metadata.csv",
+        "dataset4": "TCGA-GBM_TCGA-THCA_TCGA-LAML_TCGA-HNSC_TCGA-LUAD_TCGA-UCEC_TCGA-KIRC_TCGA-SARC__GeneExpressionQuantification_HTSeq-Counts_metadata.csv",
+    }
 
-PATH = "data/"+dataset+"/"
-METADATA_PATH = PATH + METADATA_PATHS[dataset]
-STATISTICS_PATH = PATH + "statistics/"
+    PATH = "data/"+dataset+"/"
+    METADATA_PATH = PATH + METADATA_PATHS[dataset]
+    STATISTICS_PATH = PATH + "statistics/"
 
-metadata_file = open(METADATA_PATH, "rb")
-metadata = np.genfromtxt(metadata_file, delimiter=",", dtype=np.unicode)[1:,1:]
+    metadata_file = open(METADATA_PATH, "rb")
+    metadata = np.genfromtxt(metadata_file, delimiter=",", dtype=np.unicode)[1:,1:]
 
-sample_types = np.unique(metadata[1:,:])
-sample_types = [x.strip('"') for x in sample_types]
+    sample_types = np.unique(metadata[1:,:])
+    sample_types = [x.strip('"') for x in sample_types]
 
-counts = {}
-last_cancer_type = ""
-for column in metadata.T:
-    cancer_type = column[0].strip('"').split('-', 1)[-1]
-    sample_type = column[1].strip('"')
+    counts = {}
+    last_cancer_type = ""
+    for column in metadata.T:
+        cancer_type = column[0].strip('"').split('-', 1)[-1]
+        sample_type = column[1].strip('"')
 
-    if cancer_type == 'NA':
-        cancer_type = last_cancer_type
+        if cancer_type == 'NA':
+            cancer_type = last_cancer_type
 
-    if cancer_type not in counts:
-        counts[cancer_type] = {x : 0 for x in sample_types}
-    else:
-        counts[cancer_type][sample_type] += 1
-    
-    last_cancer_type = cancer_type
+        if cancer_type not in counts:
+            counts[cancer_type] = {x : 0 for x in sample_types}
+        else:
+            counts[cancer_type][sample_type] += 1
+        
+        last_cancer_type = cancer_type
 
 
-# write statistics files
-if not os.path.exists(STATISTICS_PATH):
-    os.makedirs(STATISTICS_PATH)
+    # write statistics files
+    if not os.path.exists(STATISTICS_PATH):
+        os.makedirs(STATISTICS_PATH)
 
-np.save(STATISTICS_PATH + "sample_types", sample_types)
-np.save(STATISTICS_PATH + "counts", counts)
+    np.save(STATISTICS_PATH + "sample_types", sample_types)
+    np.save(STATISTICS_PATH + "counts", counts)
 
-with open(STATISTICS_PATH + 'counts_human_readable.md', 'w') as outfile:
-    # HEADER
-    outfile.write("| CANCER TYPE ")
-    for sample_type in sample_types:
-        outfile.write("| "+ sample_type)
-    outfile.write(" |\n")
-
-    # SEPARATION LINE
-    outfile.write("| ---- ")
-    for sample_type in sample_types:
-        outfile.write("| ---- ")
-    outfile.write(" |\n")
-
-    # DATA
-    for sample_type in counts:
-        outfile.write("| "+sample_type)
-        for count in counts[sample_type].values():
-            outfile.write("| " + str(count))
+    with open(STATISTICS_PATH + 'counts_human_readable.md', 'w') as outfile:
+        # HEADER
+        outfile.write("| CANCER TYPE ")
+        for sample_type in sample_types:
+            outfile.write("| "+ sample_type)
         outfile.write(" |\n")
 
-# create plot for sample types by cancer type
-fig, ax = plt.subplots()
-index = np.arange(len(counts))
-bar_width = 0.1
-colors = ["blue","red","green","yellow","orange","black","grey","magenta","cyan"]
-colors = colors[:len(sample_types)]
-for i in range(0, len(sample_types)):
-    values = [x[sample_types[i]] for x in counts.values()]
-    plt.bar(index + (i * bar_width), values, bar_width, color=colors[i], label=sample_types[i])
+        # SEPARATION LINE
+        outfile.write("| ---- ")
+        for sample_type in sample_types:
+            outfile.write("| ---- ")
+        outfile.write(" |\n")
 
-plt.xlabel('Cancer Type')
-plt.ylabel('Counts')
-plt.title('Sample types by cancer type')
-plt.xticks(index + bar_width / 2, counts.keys())
-plt.legend()
-plt.tight_layout()
-plt.savefig(STATISTICS_PATH + 'plot_sample_types_by_cancer_type.png')
+        # DATA
+        for sample_type in counts:
+            outfile.write("| "+sample_type)
+            for count in counts[sample_type].values():
+                outfile.write("| " + str(count))
+            outfile.write(" |\n")
+
+    # create plot for sample types by cancer type
+    fig, ax = plt.subplots()
+    index = np.arange(len(counts))
+    bar_width = 0.1
+    colors = ["blue","red","green","yellow","orange","black","grey","magenta","cyan"]
+    colors = colors[:len(sample_types)]
+    for i in range(0, len(sample_types)):
+        values = [x[sample_types[i]] for x in counts.values()]
+        plt.bar(index + (i * bar_width), values, bar_width, color=colors[i], label=sample_types[i])
+
+    plt.xlabel('Cancer Type')
+    plt.ylabel('Counts')
+    plt.title('Sample types by cancer type')
+    plt.xticks(index + bar_width / 2, counts.keys())
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(STATISTICS_PATH + 'plot_sample_types_by_cancer_type.png')
 
 
-# create plot for sick and healthy by cancer type
-fig, ax = plt.subplots()
-index = np.arange(len(counts))
-bar_width = 0.2
+    # create plot for sick and healthy by cancer type
+    fig, ax = plt.subplots()
+    index = np.arange(len(counts))
+    bar_width = 0.2
 
-sick = []
-healthy = []
-for sample_type in sample_types:
-    if sample_type.startswith("T"):
-        sick.append([x[sample_type] for x in counts.values()])
-    elif sample_type.startswith("N"):
-        healthy.append([x[sample_type] for x in counts.values()])
-    else:
-        print("sample type " + sample_type + " is not included")
+    sick = []
+    healthy = []
+    for sample_type in sample_types:
+        if sample_type.startswith("T"):
+            sick.append([x[sample_type] for x in counts.values()])
+        elif sample_type.startswith("N"):
+            healthy.append([x[sample_type] for x in counts.values()])
+        else:
+            print("sample type " + sample_type + " is not included")
 
-sick = np.asarray(sick)
-sick = sick.sum(axis=0)
-healthy = np.asarray(healthy)
-healthy = healthy.sum(axis=0)
+    sick = np.asarray(sick)
+    sick = sick.sum(axis=0)
+    healthy = np.asarray(healthy)
+    healthy = healthy.sum(axis=0)
 
-plt.bar(index, sick, bar_width, color='r', label='sick')
-plt.bar(index + bar_width, healthy, bar_width, color='b', label='healthy')
+    plt.bar(index, sick, bar_width, color='r', label='sick')
+    plt.bar(index + bar_width, healthy, bar_width, color='b', label='healthy')
 
-plt.xlabel('Cancer Type')
-plt.ylabel('Counts')
-plt.title('Sick and healthy by cancer type')
-plt.xticks(index + bar_width / 2, counts.keys())
-plt.legend()
-plt.tight_layout()
-plt.savefig(STATISTICS_PATH + 'plot_sick_and_healthy_by_cancer_type.png')
+    plt.xlabel('Cancer Type')
+    plt.ylabel('Counts')
+    plt.title('Sick and healthy by cancer type')
+    plt.xticks(index + bar_width / 2, counts.keys())
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig(STATISTICS_PATH + 'plot_sick_and_healthy_by_cancer_type.png')
--- scripts/download_data.py
+++ scripts/download_data.py
@@ -3,23 +3,28 @@ import os
 import zipfile
 import sys
 from parse_dataset import parse_dataset
+from create_statistics import create_statistics
 #sys.path.insert(0, './parse_data_scripts')
 
 urls = {
 	"dataset1": {
 		"parse": False,
+		"create_statistics": False,
 	},
 	"dataset2": {
 		"url":"https://www.dropbox.com/sh/1v31yeu0zb4jcmm/AAAi5medvxa_kIbAEB7Edywua?dl=1&preview=LargeSets.zip",
 		"parse": False,
+		"create_statistics": False,
 	},
 	"dataset3":	{
 		"url":"https://www.dropbox.com/s/0hqq2g2ipefy1u1/large_allSampleTypes.zip?dl=1",
 		"parse": True,
+		"create_statistics": True,
 	},
 	"dataset4":	{
 		"url":"https://www.dropbox.com/s/l2bpf4lka8jxktg/LargeSet.zip?dl=1",
 		"parse": True,
+		"create_statistics": True,
 	}
 }
 
@@ -48,5 +53,14 @@ for dataset, url in urls.items():
 			print("parser for " + dataset + " not implemented yet. Stay tuned!")
 	else:
 		print("already parsed " + dataset)
+
+	if not os.path.exists(path + "/statistics"):
+		if url["create_statistics"]:
+			print("creating statistics for " + dataset)
+			create_statistics(dataset)
+		else:
+			print("skipping statistics for " + dataset)
+	else:
+		print("already created statistics for " + dataset)
 
 	print("finished " + dataset)
