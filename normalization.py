665314c9f36d9a5f8cd0242d0294757fa58b6e44
SebastianRehfeldt <sebastian.rehfeldt@gmx.net>
2017-11-23 15:00:07 +0100

    committed bad working normalization

--- normalization.py
+++ normalization.py
@@ -2,6 +2,7 @@
 import numpy as np
 from utils.DataLoader import DataLoader
 from utils.DimensionalityReducer import DimensionalityReducer
+from utils.plot import plotScatter
 print("Imported modules")
 
 #%%
@@ -18,11 +19,34 @@ print("got combined data")
 
 # %%
 # Feature Selection
-healthy_X, healthy_fs_indices = dimensionalityReducer.getFeatures(healthy_data, healthy_labels, 100)
-sick_X, sick_fs_indices = dimensionalityReducer.getFeatures(sick_data, sick_labels, 100)
+healthy_X, healthy_fs_indices = dimensionalityReducer.getFeatures(healthy_data, healthy_labels, 3)
+sick_X, sick_fs_indices = dimensionalityReducer.getFeatures(sick_data, sick_labels, 3)
 print("feature selection done")
 
 # compare selected genes
 intersection = np.intersect1d(healthy_fs_indices, sick_fs_indices)
 print(intersection.size)
 print(gene_labels[intersection])
+
+#%%
+plotScatter(healthy_X,healthy_colors,healthy_labels)
+plotScatter(sick_X,sick_colors,sick_labels)
+
+healthy_X2 = healthy_data[:, sick_fs_indices]
+sick_X2 = sick_data[:, healthy_fs_indices]
+
+plotScatter(healthy_X2,healthy_colors,healthy_labels)
+plotScatter(sick_X2,sick_colors,sick_labels)
+
+#%%
+sick_normalized = sick_data - np.mean(healthy_data)
+sick_normalized = sick_normalized + np.absolute(np.min(sick_normalized))
+sick_X_norm, sick_fs_indices_norm = dimensionalityReducer.getFeatures(sick_normalized, sick_labels, 3)
+plotScatter(sick_X_norm,sick_colors,sick_labels)
+
+healthy_X3 = healthy_data[:,sick_fs_indices_norm]
+plotScatter(healthy_X3,healthy_colors,healthy_labels)
+
+#%%
+print(np.intersect1d(sick_fs_indices_norm, healthy_fs_indices))
+print(gene_labels[588])
