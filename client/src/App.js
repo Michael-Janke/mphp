47a06d0aaa65f0a5595ca0f82130a833a5fcd8f8
Tamara Slosarek <tamaraslosarek@gmail.com>
2017-11-24 11:10:53 +0100

    Adaptions to masterproject

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from './assets/images/logo.png';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app" style={styles.app}>
+        <header className="header" style={styles.header}>
+          <img src={logo} className="logo" alt="logo" style={styles.logo} />
+          <h1 className="title" style={styles.title}>Epic Masterproject</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+        <p className="intro" style={styles.intro}>
+          We're doing awesome stuff.
         </p>
       </div>
     );
   }
+}
+
+const styles = {
+  app: {
+    textAlign: "center",
+  },
+  logo: {
+    height: "80px",
+  },
+  header: {
+    backgroundColor: "#222",
+    height: "150px",
+    padding: "20px",
+    color: "white",
+  },
+  title: {
+    fontSize: "1.5em",
+  },
+  intro: {
+    fontSize: "large",
+  },
 }
 
 export default App;
