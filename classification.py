594fd25b77d075e13a54cfc298a79cf900c03bd3
SebastianRehfeldt <sebastian.rehfeldt@gmx.net>
2017-12-01 11:18:14 +0100

    added classification script

---
+++ classification.py
@@ -0,0 +1,50 @@
+#%%
+import numpy as np
+from pprint import pprint
+from utils.DataLoader import DataLoader
+from utils.DimensionalityReducer import DimensionalityReducer
+from utils.DataNormalizer import DataNormalizer
+from utils.plot import plotScatter
+
+from utils import Expressions
+
+from validation.ClusterValidator import ClusterValidator
+from validation.ClassificationValidator import ClassificationValidator
+
+print("Imported modules")
+
+#%%
+
+dataLoader = DataLoader("dataset4")
+dimReducer = DimensionalityReducer()
+
+clusVal = ClusterValidator()
+classVal = ClassificationValidator()
+print("data loaded")
+
+#%%
+healthy = dataLoader.getData(["healthy"], ["THCA","LUAD","KIRC","HNSC"])
+sick = dataLoader.getData(["sick"], ["THCA","LUAD","KIRC","HNSC"])
+gene_labels = dataLoader.getGeneLabels()
+print("got combined data")
+
+# %%
+# Feature Selection
+healthy_fs_indices, sick_X, healthy_X = dimReducer.getNormalizedFeatures(sick,healthy,"exclude", 20)
+
+sick_reduced = Expressions(sick_X, sick.labels)
+healthy_reduced = Expressions(healthy_X, healthy.labels)
+
+#pprint(clusVal.evaluate(sick_reduced, ["*"], ["*"]))
+
+print("\n\nSICK COMPLETE")
+pprint(classVal.evaluate(sick, ["LogisticRegression"]))
+print("\n################")
+
+print("SICK REDUCED")
+pprint(classVal.evaluate(sick_reduced, ["LogisticRegression"]))
+plotScatter(sick_X,sick.labels)
+
+print("HEALTHY REDUCED")
+pprint(classVal.evaluate(healthy_reduced, ["LogisticRegression"]))
+plotScatter(healthy_X,healthy.labels)
