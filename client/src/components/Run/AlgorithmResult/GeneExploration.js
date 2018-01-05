031549950dbc1fb8611f92a48277caec28019a22
Antje Schubotz <antje.schubotz@gmail.com>
2018-01-05 13:42:53 +0100

    add expression matrix to result

--- client/src/components/Run/AlgorithmResult/GeneExploration.js
+++ client/src/components/Run/AlgorithmResult/GeneExploration.js
@@ -1,29 +1,84 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn
+} from "material-ui/Table";
 
 export default class GeneExploration extends Component {
   render() {
-    const { genes, geneNames } = this.props.result;
+    const { genes, geneNames, expressionMatrix } = this.props.result;
+
     return (
       <div>
-        <h3>Gene Exploration</h3>
+        <h3>
+          {expressionMatrix === null
+            ? "Gene Exploration"
+            : "Gene Expression Levels"}
+        </h3>
         <p>
           The following genes were especially relevant for discriminating the
           clusters:
         </p>
-        <StyledList>
-          {
-            genes.map((gene, i) => (
-            <StyledItem key={gene}>
-              <a
-                href={`https://www.proteinatlas.org/${gene}`}
-                target="_blank"
-              >
-                {geneNames[i]}
-              </a>
-            </StyledItem>
-          ))}
-        </StyledList>
+        {expressionMatrix === null ? (
+          <StyledList>
+            {genes.map((gene, i) => (
+              <StyledItem key={gene}>
+                <a
+                  href={`https://www.proteinatlas.org/${gene}`}
+                  target="_blank"
+                >
+                  {geneNames[i]}
+                </a>
+              </StyledItem>
+            ))}
+          </StyledList>
+        ) : (
+          <Table bodyStyle={{ overflow: "visible" }}>
+            <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
+              <TableRow>
+                <TableHeaderColumn />
+                {genes.map((gene, i) => (
+                  <TableHeaderColumn>
+                    <a
+                      href={`https://www.proteinatlas.org/${gene}`}
+                      target="_blank"
+                    >
+                      {geneNames[i]}
+                    </a>
+                  </TableHeaderColumn>
+                ))}
+              </TableRow>
+            </TableHeader>
+            <TableBody displayRowCheckbox={false}>
+              {Object.entries(expressionMatrix).map(
+                ([cancerType, expressions]) => {
+                  return (
+                    <TableRow>
+                      <TableHeaderColumn>{cancerType}</TableHeaderColumn>
+                      {expressions.map(item => {
+                        return (
+                          <TableRowColumn>
+                            {item
+                              .replace("unchanged", "o")
+                              .replace("mid-lower", "-")
+                              .replace("mid-higher", "+")
+                              .replace("lower", "--")
+                              .replace("higher", "++")}
+                          </TableRowColumn>
+                        );
+                      })}
+                    </TableRow>
+                  );
+                }
+              )}
+            </TableBody>
+          </Table>
+        )}
       </div>
     );
   }
