43001f94209e6b49bf61649317a83b165882aba4
Antje Schubotz <antje.schubotz@gmail.com>
2018-01-25 11:24:38 +0100

    refactor app.py

---
+++ server/algorithmExecution.py
@@ -0,0 +1,98 @@
+import numpy as np
+
+from utils.DimensionalityReducer import DimensionalityReducer
+from validation.Analyzer import Analyzer
+
+dimReducer = DimensionalityReducer()
+analyzer = Analyzer()
+
+
+def getData(algorithm, dataLoader):
+    cancer_types = algorithm["cancerTypes"]
+    sick_tissue_types = algorithm["sickTissueTypes"]
+    healthy_tissue_types = algorithm["healthyTissueTypes"]
+
+    sick = dataLoader.getData(sick_tissue_types, cancer_types)
+    sick = dataLoader.replaceLabels(sick)
+    healthy = dataLoader.getData(healthy_tissue_types, cancer_types)
+    healthy = dataLoader.replaceLabels(healthy)
+    combined = dataLoader.getData(
+        sick_tissue_types + healthy_tissue_types, cancer_types)
+    combined = dataLoader.replaceLabels(combined)
+
+    data = {
+        "sick": sick,
+        "healthy": healthy,
+        "combined": combined
+    }
+    return data
+
+
+def run(algorithm, data):
+    key = algorithm["key"]
+    n_components = algorithm["parameters"].get("n_components")
+    n_f_components = algorithm["parameters"].get("n_features_per_component")
+    k = algorithm["parameters"].get("k")
+    n = algorithm["parameters"].get("n")
+
+    if key == "getPCA":
+        gene_indices = dimReducer.getPCA(
+            data["combined"].expressions, n_components, n_f_components)
+        labels = data["combined"].labels
+
+    elif key == "getFeatures":
+        gene_indices = dimReducer.getFeatures(data["combined"], k)
+        labels = data["combined"].labels
+
+    elif key == "getDecisionTreeFeatures":
+        gene_indices = dimReducer.getDecisionTreeFeatures(data["combined"], k)
+        labels = data["combined"].labels
+
+    elif key == "getNormalizedFeaturesE":
+        gene_indices = dimReducer.getNormalizedFeaturesE(
+            data["sick"], data["healthy"], k, n, "chi2")
+        labels = np.hstack((data["sick"].labels, data["healthy"].labels))
+
+    elif key == "getNormalizedFeaturesS":
+        gene_indices = dimReducer.getNormalizedFeaturesS(
+            data["sick"], data["healthy"], k, n, "chi2")
+        labels = np.hstack((data["sick"].labels, data["healthy"].labels))
+
+    elif key == "getFeaturesBySFS":
+        gene_indices = dimReducer.getFeaturesBySFS(
+            data["sick"], data["healthy"])
+        labels = np.hstack((data["sick"].labels, data["healthy"].labels))
+
+    X = data["combined"].expressions[:, gene_indices]
+
+    response_data = {}
+    for label in np.unique(labels):
+        response_data[label] = X[labels == label, :].T.tolist()
+
+    return response_data, gene_indices
+
+
+def calcExpressionMatrix(algorithm, data, gene_indices):
+    key = algorithm["key"]
+    # only calc expression matrix if algorithm result allows it
+    allowedAlgorithms = ["getNormalizedFeaturesE",
+                         "getNormalizedFeaturesS", "getFeaturesBySFS"]
+    if key not in allowedAlgorithms:
+        return None
+
+    X = np.vstack(
+        (data["sick"].expressions[:, gene_indices], data["healthy"].expressions[:, gene_indices]))
+
+    return analyzer.computeExpressionMatrix(data["sick"], data["healthy"], gene_indices)
+
+
+def evaluate(algorithm, data, gene_indices):
+    cancer_types = algorithm["cancerTypes"]
+    sick_tissue_types = algorithm["sickTissueTypes"]
+    healthy_tissue_types = algorithm["healthyTissueTypes"]
+
+    if len(cancer_types) == 1 or len(sick_tissue_types) == 0 or len(healthy_tissue_types) == 0:
+        return analyzer.computeFeatureValidation(data["combined"], "", gene_indices)
+    else:
+        return analyzer.computeFeatureValidation(
+            data["sick"], data["healthy"], gene_indices)
---
+++ server/externalApiCalls.py
@@ -0,0 +1,139 @@
+import pandas as pd
+import numpy as np
+import os.path
+import json
+import urllib
+
+from SPARQLWrapper import SPARQLWrapper, JSON
+from xml.dom.minidom import parse, parseString
+
+
+def getCancerGeneCensusData():
+    csv_file = 'data/cancer_gene_census.csv'
+    return pd.read_csv(csv_file)
+
+
+def lookupDisgenet(gene):
+    entrez_file = "data/gene_names/entrez_names.npy"
+    entrez_labels_map = np.load(entrez_file).item()
+    gene_url = '<http://identifiers.org/ncbigene/' + \
+        entrez_labels_map[int(gene[4:])] + '>'
+    file_name = 'data/disgenet/' + gene + '.json'
+    results = None
+    try:
+        if not os.path.isfile(file_name):
+            sparql = SPARQLWrapper('http://rdf.disgenet.org/sparql/')
+            sparql.setQuery("""
+            SELECT DISTINCT
+                ?gda
+                %s as ?gene
+                ?score
+                ?disease
+                ?diseaselabel
+                ?diseasename
+                ?semanticType
+            WHERE {
+                ?gda sio:SIO_000628 %s, ?disease ;
+                    sio:SIO_000253 ?source ;
+                    sio:SIO_000216 ?scoreIRI .
+                ?disease sio:SIO_000008 ?semanticType .
+                ?disease a ncit:C7057 .
+                ?scoreIRI sio:SIO_000300 ?score .
+                FILTER regex(?source, "UNIPROT|CTD_human")
+                FILTER (?score >= 0.2)
+                ?disease dcterms:title ?diseasename .
+                ?disease rdfs:label ?diseaselabel
+            }
+            ORDER BY DESC(?score)
+            """ % (gene_url, gene_url))
+            sparql.setReturnFormat(JSON)
+            results = sparql.query().convert()
+            if not os.path.exists(os.path.dirname(file_name)):
+                os.makedirs(os.path.dirname(file_name))
+            with open(file_name, 'w') as outfile:
+                json.dump(results, outfile)
+
+        f = open(file_name, 'r')
+        results = json.load(f)
+        f.close()
+    except:
+        print("Error getting / reading disgenet file")
+
+    if results is not None:
+
+        # scan the file for a T191 --> cancer.
+        json_data = results
+
+        def search(key, var):
+            if hasattr(var, 'items'):
+                for k, v in var.items():
+                    if k == key:
+                        yield v
+                    if isinstance(v, dict):
+                        for result in search(key, v):
+                            yield result
+                    elif isinstance(v, list):
+                        for d in v:
+                            for result in search(key, d):
+                                yield result
+
+        semantic_types = search('semanticType', json_data)
+        for semantic_type in semantic_types:
+            for k, v in semantic_type.items():
+                if k == 'value':
+                    if 'T191' in v:
+                        return True
+    return False
+
+
+def lookupProteinAtlas(gene):
+    try:
+        url = 'https://www.proteinatlas.org/' + gene + '.xml'
+        file_name = 'data/proteinatlas/' + gene + '.xml'
+        if not os.path.exists(os.path.dirname(file_name)):
+            os.makedirs(os.path.dirname(file_name))
+        if not os.path.isfile(file_name):
+            urllib.request.urlretrieve(url, file_name)
+
+        f = open(file_name, 'r')
+        xml = f.read()
+        f.close()
+        dom = parseString(xml)
+        proteinClass = dom.getElementsByTagName('proteinClass')
+        for c in proteinClass:
+            if c.attributes['name'].value == "Cancer-related genes":
+                return True
+    except:
+        print("Error accessing proteinatlas for gene: " + gene)
+    return False
+
+
+def lookupCancerGeneCensus(gene, cancer_gene_census_data):
+    census_data_genes = cancer_gene_census_data['Synonyms'].tolist()
+    for gene_synonyms in census_data_genes:
+        # filter out NaN values
+        if gene_synonyms == gene_synonyms:
+            if gene in gene_synonyms:
+                return True
+    return False
+
+
+def testGenes(request):
+    data = request.get_json()["genes"]
+    genes = data["genes"]
+    response = {}
+
+    cancer_gene_census_data = getCancerGeneCensusData()
+
+    for gene in genes:
+        disgenet = lookupDisgenet(gene)
+        proteinAtlas = lookupProteinAtlas(gene)
+        cancerGeneCensus = lookupCancerGeneCensus(
+            gene, cancer_gene_census_data)
+
+        score = (0.5 if cancerGeneCensus else 0) + \
+            (0.25 if disgenet else 0) + (0.25 if proteinAtlas else 0)
+        response[gene] = {'proteinAtlas': proteinAtlas, 'disgenet': disgenet,
+                          'cancer_gene_census': cancerGeneCensus, 'score': score}
+
+    return response
