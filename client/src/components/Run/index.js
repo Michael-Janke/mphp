a17472b3b0370d572bae71d253546e0258c74863
Antje Schubotz <antje.schubotz@gmail.com>
2018-02-01 10:20:31 +0100

    style delete action of cards

--- client/src/components/Run/index.js
+++ client/src/components/Run/index.js
@@ -1,48 +1,49 @@
 import React, { Component } from "react";
 import { Card as _Card, CardTitle } from "material-ui/Card";
-import FlatButton from 'material-ui/FlatButton';
-import Dialog from 'material-ui/Dialog';
+import FlatButton from "material-ui/FlatButton";
+import Dialog from "material-ui/Dialog";
 import styled from "styled-components";
 
 import Spinner from "../Spinner";
 import IconButton from "../IconButton";
 import AlgorithmExecution from "./AlgorithmExecution";
 import AlgorithmResult from "./AlgorithmResult";
-import deepEqual from 'deep-equal';
+import deepEqual from "deep-equal";
 
 const CARD_TITLE_HEIGHT = 15;
 
 export default class Card extends Component {
-
   state = {
-    open: false,
+    open: false
   };
 
   shouldComponentUpdate(nextProps, nextState) {
-    if(nextState.open !== this.state.open) return true;    
-    if(this.props.geneResult) return false; // will never change again
-    if(nextProps.isLoading !== this.props.isLoading) return true;
-    if(nextProps.dataset !== this.props.dataset) return true;
-    if(nextProps.result && !this.props.result) return true;
-    if(nextProps.geneResult && !this.props.geneResult) return true;
+    if (nextState.open !== this.state.open) return true;
+    if (this.props.geneResult) return false; // will never change again
+    if (nextProps.isLoading !== this.props.isLoading) return true;
+    if (nextProps.dataset !== this.props.dataset) return true;
+    if (nextProps.result && !this.props.result) return true;
+    if (nextProps.geneResult && !this.props.geneResult) return true;
     return !deepEqual(nextProps.algorithm, this.props.algorithm);
   }
 
   handleOpen = () => {
-    this.setState({open: true});
+    this.setState({ open: true });
   };
 
   handleClose = (deleteIt = false) => {
-    if(deleteIt) { this.props.deleteRun(this.props.runId); }
-    this.setState({open: false});
+    if (deleteIt) {
+      this.props.deleteRun(this.props.runId);
+    }
+    this.setState({ open: false });
   };
 
   startRun = () => {
     this.props.startRun(this.props.runId, {
       ...this.props.algorithm,
       dataset: this.props.dataset
-    })
-  }
+    });
+  };
 
   render() {
     const { isLoading, algorithm, datasets, dataset, result } = this.props;
@@ -50,25 +51,41 @@ export default class Card extends Component {
       <StyledCard zDepth={1}>
         <StyledCardTitle>
           <StyledTitleText>
-          {datasets[dataset] || ""} | {algorithm.name || "Execute Algorithm"}
+            {datasets[dataset] || ""} | {algorithm.name || "Execute Algorithm"}
           </StyledTitleText>
           {isLoading ? <Spinner size={CARD_TITLE_HEIGHT} /> : null}
-          <IconButton icon="delete" onClick={this.handleOpen} />
+          <IconButton icon="clear" onClick={this.handleOpen} />
           <Dialog
-              actions={[<FlatButton label="No" primary={false}  onClick={this.handleClose.bind(this,false)} />,
-                        <FlatButton label="Yes" primary={true}  onClick={this.handleClose.bind(this,true)} />]}
-              modal={false}
-              open={this.state.open}
-              onRequestClose={this.handleClose.bind(this,false)}
-            >
-              Do you really want to delete this run?
+            actions={[
+              <FlatButton
+                label="No"
+                primary={false}
+                onClick={this.handleClose.bind(this, false)}
+              />,
+              <FlatButton
+                label="Yes"
+                primary={true}
+                onClick={this.handleClose.bind(this, true)}
+              />
+            ]}
+            modal={false}
+            open={this.state.open}
+            onRequestClose={this.handleClose.bind(this, false)}
+          >
+            Do you really want to delete this run?
           </Dialog>
         </StyledCardTitle>
         {this.renderBody()}
-        {result && result.error ?
-          <StyledError> {result.error.message} <FlatButton label="Retry" primary={true}  onClick={this.startRun.bind(this)} /> </StyledError>
-        : null
-        }
+        {result && result.error ? (
+          <StyledError>
+            {result.error.message}
+            <FlatButton
+              label="Retry"
+              primary={true}
+              onClick={this.startRun.bind(this)}
+            />
+          </StyledError>
+        ) : null}
       </StyledCard>
     );
   }
@@ -99,18 +116,20 @@ const StyledCard = styled(_Card)`
   padding-bottom: ${props => props.theme.smallerSpace};
   min-width: 800px;
   max-width: 1000px;
-  flex:1;
+  flex: 1;
 `;
 
 const StyledCardTitle = styled(CardTitle)`
   display: flex;
   align-items: center;
   height: ${CARD_TITLE_HEIGHT}px;
+  padding-right: 0 !important;
 `;
 
 const StyledTitleText = styled.p`
   margin-right: ${props => props.theme.mediumSpace};
   font-size: ${props => props.theme.h2};
+  flex: 1;
 `;
 
 const StyledError = styled.div`
